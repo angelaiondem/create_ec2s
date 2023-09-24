4807b4e010e8ad9d98d7512acf2a63ebaa9b546e
angelaiondem <angelaiondem@yahoo.com>
2023-09-24 21:40:13 +0200

    Add IAM Role

--- main_test_env.tf
+++ main_test_env.tf
@@ -45,14 +45,14 @@ resource "aws_security_group" "elb" {
 
 # Launch EC2 instances
 resource "aws_instance" "test_env_instance" {
-  count           = 2
-  ami             = var.ami
-  instance_type   = "t2.micro"
-  subnet_id       = var.public-subnets2_id
-  security_groups = [aws_security_group.test-env-sg.id]
+  count                = 2
+  ami                  = var.ami
+  instance_type        = "t2.micro"
+  subnet_id            = var.public-subnets2_id
+  security_groups      = [aws_security_group.test-env-sg.id]
   iam_instance_profile = aws_iam_instance_profile.test-env-admin-role.name
-  key_name  = "devops-project-test-env-key"
-  user_data = file("user_data.sh")
+  key_name             = "devops-project-test-env-key"
+  user_data            = file("user_data.sh")
   tags = {
     Name = "Test-env-instance-${count.index + 1}"
   }
