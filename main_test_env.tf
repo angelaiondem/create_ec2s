579cdb651b8dc66a858c4276917beb0a5777a1a5
angelaiondem <angelaiondem@yahoo.com>
2023-09-24 18:24:32 +0200

    Add egress in SG.

--- main_test_env.tf
+++ main_test_env.tf
@@ -20,6 +20,13 @@ resource "aws_security_group" "test-env-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  egress {
+      from_port   = 0
+      to_port     = 0
+      protocol    = -1
+      cidr_blocks = ["0.0.0.0/0"]
+    }
 }
 
 
