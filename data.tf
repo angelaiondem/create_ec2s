10f3e5084ef379d467cc111d2b20dcb1c0b21647
angelaiondem <angelaiondem@yahoo.com>
2023-09-24 14:44:38 +0200

    Add Test env code.

---
+++ data.tf
@@ -0,0 +1,12 @@
+# data "aws_vpc" "main_vpc_id" {
+#   id = var.project_vpc_id
+# }
+
+
+# data "aws_subnet" "subnet1" {
+#   id = var.public-subnets1_id
+# }
+
+# data "aws_subnet" "subnet2" {
+#   id = var.public-subnets2_id
+# }
---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "region" {
+    default = "eu-central-1"
+}
+
+variable "project_vpc_id" {
+    default = "vpc-0a586e5b5708fff72"
+}
+
+variable "ami" {
+    default = "ami-0ab1a82de7ca5889c"
+}
+
+variable "public-subnets2_id" {
+    default = "subnet-05656f14c922bfbb0"
+}
