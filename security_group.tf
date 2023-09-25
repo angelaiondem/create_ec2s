5752c0a54590651711f655030cdad4deb47ef98b
angelaiondem <angelaiondem@yahoo.com>
2023-09-26 00:12:34 +0200

    Add ingress port in SG

--- security_group.tf
+++ security_group.tf
@@ -94,6 +94,14 @@ resource "aws_security_group" "ec2_sg_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Allow SSH from the local network."
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
