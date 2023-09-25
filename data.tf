309b1a66093e16a71ca0b0d7dd67b9403c24ed68
angelaiondem <angelaiondem@yahoo.com>
2023-09-25 10:50:09 +0200

    Add data file

--- data.tf
+++ data.tf
@@ -1,12 +1,1 @@
-# data "aws_vpc" "main_vpc_id" {
-#   id = var.project_vpc_id
-# }
-
-
-# data "aws_subnet" "subnet1" {
-#   id = var.public-subnets1_id
-# }
-
-# data "aws_subnet" "subnet2" {
-#   id = var.public-subnets2_id
-# }
+data "aws_availability_zones" "available" {}
